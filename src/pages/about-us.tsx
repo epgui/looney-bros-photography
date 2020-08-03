8cd877819358f947c53a45d0a2040bdd296b9642
Guillaume <guigui.p@gmail.com>
2020-08-02 22:09:45 -0700

    Lorem ipsum.

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -38,6 +38,8 @@ const About: React.FC<Props> = props => {
       >
         <h1>About us</h1>
 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
         <Bio
           name="Guillaume Pelletier"
           title="Partner"
@@ -53,7 +55,7 @@ const About: React.FC<Props> = props => {
           title="Partner"
           image={seb}
         >
-          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Bio>
       </AnimatedBox>
     </Layout>
